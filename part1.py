6c696422cd8c518e54b05d8ea7a663308e172e2e
cincy sebastian <cincy123@gmail.com>
2024-05-17 18:44:33 -0600

    mod1assignments

---
+++ part1.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+result_addition = num1 + num2
+result_subtraction = num1 - num2
+print("Addition Result:", result_addition)
+print("Subtraction Result:", result_subtraction)
---
+++ part2.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+result_multiplication = num1 * num2
+result_division = num1 / num2
+print("Multiplication Result:", result_multiplication)
+print("Division Result:", result_division)
